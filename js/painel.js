6d8603eafb3db780bd4d0ad9be8d615dfd5c84e0
Lucas Pinheiro <pinheirolouco@gmail.com>
2013-04-09 11:48:31 -0300

    versao 09/04/2013 i

--- js/painel.js
+++ js/painel.js
@@ -14,39 +14,4 @@ $(document).on("pageinit", function() {
         verificar_tabelas();
         _session.clear();
     });
-    document.addEventListener("deviceready", onDeviceReady, true);
 });
-
-// Wait for Cordova to load
-// 
-
-
-// Cordova is loaded and it is now safe to make calls Cordova methods
-//
-function onDeviceReady() {
-    alert('aaa');
-    alert(device);
-    _session.set("config_device_name", device.name);
-    _session.set("config_device_cordova", device.cordova);
-    _session.set("config_device_platform", device.platform);
-    _session.set("config_device_uuid", device.uuid);
-    _session.set("config_device_model", device.model);
-    _session.set("config_device_version", device.version);
-    checkConnection();
-}
-
-function checkConnection() {
-    var networkState = navigator.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN] = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI] = 'WiFi connection';
-    states[Connection.CELL_2G] = 'Cell 2G connection';
-    states[Connection.CELL_3G] = 'Cell 3G connection';
-    states[Connection.CELL_4G] = 'Cell 4G connection';
-    states[Connection.NONE] = 'No network connection';
-
-    _session.set("config_conexao_nome", states[networkState]);
-    _session.set("config_conexao_id", networkState);
-}
