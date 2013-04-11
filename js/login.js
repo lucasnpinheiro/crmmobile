d934116f706e330ca1cd480ab2c1eea2124f7f28
Lucas Pinheiro <pinheirolouco@gmail.com>
2013-04-11 19:15:21 -0300

    versao 11/04/2013 b

--- js/login.js
+++ js/login.js
@@ -2,20 +2,9 @@ $(document).on("pageinit", function() {
     if ( _session.get("usuario") != "" && _session.get("usuario") != undefined && _session.get("usuario") != null ) {
         _constant.redirect('painel.html');
     } else {
-        var c = 'SELECT codigo_ativacao FROM empresas;';
-        db.transaction(function( e ) {
-            e.executeSql(c, [ ], function( g, f ) {
-                debug("SUCESSO", c);
-                debug("TOTAL", f.rows.length);
-                if ( f.rows.item(0).codigo_ativacao == '' ) {
-                    _constant.redirect("atualizacoes_ativacao.html");
-                }
-            }, function( g, f ) {
-                debug("ERROR", f.message);
-            });
-        });
+        _configuracoes.verifica_chave();
     }
-    
+
     $("#bt_logar").click(function( a ) {
         a.preventDefault();
         if ( $(this).closest("form").form_valida() == true ) {
@@ -30,34 +19,25 @@ function logar( d ) {
     var a = $(d).closest("form").find("#senha").val();
     var c = 'SELECT * FROM usuarios WHERE usuario="' + b + '" AND  senha="' + md5(a) + '"';
     db.transaction(function( e ) {
-        e.executeSql(c, [ ], function( g, f ) {
-            debug("SUCESSO", c);
-            debug("TOTAL", f.rows.length);
-            if ( f.rows.length != 0 ) {
-                debug("SUSSESO", "ID Usuário: " + f.rows.item(0).id_usuarios);
-                _session.set("id_usuarios", f.rows.item(0).id_usuarios);
-                _session.set("usuario", f.rows.item(0).usuario);
-                _session.set("nivel", f.rows.item(0).nivel);
-                _constant.redirect("painel.html");
-            } else {
-                insert_usuarios(b, a, d)
-            }
-        }, function( g, f ) {
-            debug("ERROR", f.message);
-            verificar_tabelas()
-        })
-    })
+        e.executeSql(c, [ ],
+                function( g, f ) {
+                    debug("QUERY", c);
+                    debug("TOTAL", f.rows.length);
+                    if ( f.rows.length != 0 ) {
+                        debug("SUSSESO", "ID Usuário: " + f.rows.item(0).id_usuarios);
+                        _session.set("id_usuarios", f.rows.item(0).id_usuarios);
+                        _session.set("usuario", f.rows.item(0).usuario);
+                        _session.set("nivel", f.rows.item(0).nivel);
+                        _constant.redirect("painel.html");
+                    } else {
+                        jAviso('Usuário não localizado.');
+                    }
+                },
+                function( g, f ) {
+                    debug("QUERY", c);
+                    debug("ERROR", f.message);
+                    _configuracoes.verifica_tabelas();
+                });
+    });
 }
-function insert_usuarios( b, a, d ) {
-    var c = 'INSERT INTO usuarios (id_empresas, cod_usuario, dsc_usuario, usuario, nome, senha) VALUES (1, "123", "' + b + '", "' + b + '", "' + b + '",  "' + md5(a) + '",  "2");';
-    db.transaction(function( e ) {
-        e.executeSql(c, [ ], function( g, f ) {
-            debug("SUCESSO", c);
-            debug("TOTAL", f.rows.length);
-            logar(d)
-        }, function( g, f ) {
-            debug("ERROR", f.message);
-            verificar_tabelas()
-        })
-    })
-}
+
--- js/model.js
+++ js/model.js
@@ -1,6 +1,21 @@
-if ( db != null ) {
-    function verificar_tabelas( ) {
-        var a = [ {
+_configuracoes = {
+    tabelas : {
+        create : [ {
+                tbl : "CREATE TABLE IF NOT EXISTS empresas ( id_empresas INTEGER PRIMARY KEY AUTOINCREMENT, uuid VARCHAR, codigo_cliente VARCHAR, codigo_ativacao VARCHAR, cpf_cnpj VARCHAR(14), nome_empresa VARCHAR(50), data_hora_cadastro TEXT, data_hora_exclusao TEXT )"
+            }, {
+                tbl : "CREATE TABLE IF NOT EXISTS equipamentos ( id_equipamentos INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, imei VARCHAR(100) )"
+            }, {
+                tbl : "CREATE TABLE IF NOT EXISTS clientes ( id_clientes INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, cod_cliente VARCHAR(50), dsc_cliente VARCHAR(45), data_hora_atualizacao TEXT, data_hora_exclusao TEXT )"
+            }, {
+                tbl : "CREATE TABLE IF NOT EXISTS produtos ( id_produtos INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, cod_produto VARCHAR(50), dsc_produto VARCHAR(100), estoque REAL (10,5) DEFAULT 0, desconto_maximo REAL (10,2) DEFAULT 0, data_hora_atualizacao TEXT, valor REAL (10,2) )"
+            }, {
+                tbl : "CREATE TABLE IF NOT EXISTS usuarios ( id_usuarios INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, cod_usuario VARCHAR(50), dsc_usuario VARCHAR(50) , usuario VARCHAR(50), nome VARCHAR(50), senha VARCHAR(32), nivel INTEGER, data_hora_atualizacao TEXT, data_hora_exclusao TEXT )"
+            }, {
+                tbl : "CREATE TABLE IF NOT EXISTS pedidos ( id_pedidos INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, id_clientes INTEGER, id_equipamentos INTEGER, id_usuarios INTEGER, data_hora_cadastro TEXT , data_hora_finalizacao TEXT , data_hora_envio TEXT, data_hora_transmissao TEXT, data_hora_exclusao TEXT, observacao VARCHAR(255) )"
+            }, {
+                tbl : "CREATE TABLE IF NOT EXISTS pedidos_itens ( id_pedidos_itens INTEGER PRIMARY KEY AUTOINCREMENT, id_pedidos INTEGER, id_produtos INTEGER, data_hora_cadastro TEXT , quantidade VARCHAR(45), valor_unitario VARCHAR(45), data_hora_exclusao TEXT )"
+            } ],
+        drop : [ {
                 tbl : "DROP TABLE IF EXISTS empresas"
             }, {
                 tbl : "DROP TABLE IF EXISTS equipamentos"
@@ -16,99 +31,139 @@ if ( db != null ) {
                 tbl : "DROP TABLE IF EXISTS pedidos_itens"
             }, {
                 tbl : "DROP TABLE IF EXISTS sqlite_sequence"
-            }, {
-                tbl : "CREATE TABLE IF NOT EXISTS empresas ( id_empresas INTEGER PRIMARY KEY AUTOINCREMENT, uuid VARCHAR, codigo_cliente VARCHAR, codigo_ativacao VARCHAR, cpf_cnpj VARCHAR(14), nome_empresa VARCHAR(50), data_hora_cadastro TEXT, data_hora_exclusao TEXT )"
-            }, {
-                tbl : "CREATE TABLE IF NOT EXISTS equipamentos ( id_equipamentos INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, imei VARCHAR(100) )"
-            }, {
-                tbl : "CREATE TABLE IF NOT EXISTS clientes ( id_clientes INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, cod_cliente VARCHAR(50), dsc_cliente VARCHAR(45), data_hora_atualizacao TEXT, data_hora_exclusao TEXT )"
-            }, {
-                tbl : "CREATE TABLE IF NOT EXISTS produtos ( id_produtos INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, cod_produto VARCHAR(50), dsc_produto VARCHAR(100), estoque REAL (10,5) DEFAULT 0, desconto_maximo REAL (10,2) DEFAULT 0, data_hora_atualizacao TEXT, valor REAL (10,2) )"
-            }, {
-                tbl : "CREATE TABLE IF NOT EXISTS usuarios ( id_usuarios INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, cod_usuario VARCHAR(50), dsc_usuario VARCHAR(50) , usuario VARCHAR(50), nome VARCHAR(50), senha VARCHAR(32), nivel INTEGER, data_hora_atualizacao TEXT, data_hora_exclusao TEXT )"
-            }, {
-                tbl : "CREATE TABLE IF NOT EXISTS pedidos ( id_pedidos INTEGER PRIMARY KEY AUTOINCREMENT, id_empresas INTEGER, id_clientes INTEGER, id_equipamentos INTEGER, id_usuarios INTEGER, data_hora_cadastro TEXT , data_hora_finalizacao TEXT , data_hora_envio TEXT, data_hora_transmissao TEXT, data_hora_exclusao TEXT, observacao VARCHAR(255) )"
-            }, {
-                tbl : "CREATE TABLE IF NOT EXISTS pedidos_itens ( id_pedidos_itens INTEGER PRIMARY KEY AUTOINCREMENT, id_pedidos INTEGER, id_produtos INTEGER, data_hora_cadastro TEXT , quantidade VARCHAR(45), valor_unitario VARCHAR(45), data_hora_exclusao TEXT )"
-            } ];
-        $.each(a, function( d, c ) {
-            db.transaction(function( b ) {
-                b.executeSql(c.tbl, [ ], function( f, e ) {
-                    debug("SUCESSO", c.tbl);
-                    if ( d == (a.length - 1) ) {
-                        definir_usuario_administradores();
-                    }
-                }, function( f, e ) {
-                    debug("ERROR", e.message);
-                });
-            });
-        });
-    }
-
-    function definir_usuario_administradores() {
-        var a = [
+            } ],
+        usuarios : [
             {
                 inst : 'INSERT INTO usuarios ("id_empresas", "cod_usuario", "dsc_usuario", "usuario", "nome", "senha", "nivel") VALUES ("1", "124", "Administrador do sistema", "root", "Administrador do sistema", "' + md5('qazse') + '", "1");'
             },
             {
                 inst : 'INSERT INTO usuarios ("id_empresas", "cod_usuario", "dsc_usuario", "usuario", "nome", "senha", "nivel") VALUES ("1", "123", "Tester do sistema", "demo", "Tester do sistema", "' + md5('demo') + '", "2");'
-            } ];
-        
-        $.each(a, function( d, c ) {
+            } ]
+    },
+    drop_tabelas : function() {
+        var total = _configuracoes.tabelas.drop.length - 1;
+        $.each(_configuracoes.tabelas.drop, function( d, c ) {
             db.transaction(function( b ) {
-                b.executeSql(c.inst, [ ], function( f, e ) {
-                    debug("SUCESSO", c.inst);
-                    if ( d == 1 ) {
-                        _constant.redirect("atualizacoes_ativacao.html");
-                    }
-                }, function( f, e ) {
-                    debug("QUERY", c.inst);
-                    debug("ERROR", e.message);
-                });
+                b.executeSql(c.tbl, [ ],
+                        function( f, e ) {
+                            debug("QUERY", c.tbl);
+                            if ( total == d ) {
+                                _constant.redirect('index.html');
+                            }
+                        },
+                        function( f, e ) {
+                            debug("QUERY", c.tbl);
+                            debug("ERROR", e.message);
+                        });
             });
         });
-    }
-
-    function check_tabela() {
+    },
+    create_tabelas : function() {
+        var total = _configuracoes.tabelas.create.length - 1;
+        $.each(_configuracoes.tabelas.create, function( d, c ) {
+            db.transaction(function( b ) {
+                b.executeSql(c.tbl, [ ],
+                        function( f, e ) {
+                            debug("QUERY", c.tbl);
+                            if ( total == d ) {
+                                _configuracoes.add_usuarios();
+                            }
+                        },
+                        function( f, e ) {
+                            debug("QUERY", c.tbl);
+                            debug("ERROR", e.message);
+                        });
+            });
+        });
+    },
+    add_usuarios : function() {
+        var total = _configuracoes.tabelas.usuarios.length - 1;
+        $.each(_configuracoes.tabelas.usuarios, function( d, c ) {
+            db.transaction(function( b ) {
+                b.executeSql(c.tbl, [ ],
+                        function( f, e ) {
+                            debug("QUERY", c.tbl);
+                            if ( total == d ) {
+                                _constant.redirect('atualizacoes_ativacao.html');
+                            }
+                        },
+                        function( f, e ) {
+                            debug("QUERY", c.tbl);
+                            debug("ERROR", e.message);
+                        });
+            });
+        });
+    },
+    verifica_tabelas : function() {
         var c = 'SELECT name FROM sqlite_master WHERE type="table" AND name="usuarios";';
         db.transaction(function( e ) {
-            e.executeSql(c, [ ], function( g, f ) {
-                debug("SUCESSO", c);
-                debug("TOTAL", f.rows.length);
-                if ( f.rows.length != 0 ) {
-                    debug("SUCESSO", 'Redirecionando para o login.');
-                    check_usuario_root();
-                } else {
-                    debug("ERROR", 'Criando tabelas.');
-                    verificar_tabelas();
-                }
-            }, function( g, f ) {
-                debug("ERROR", f.message);
-                verificar_tabelas();
-            });
+            e.executeSql(c, [ ],
+                    function( g, f ) {
+                        debug("QUERY", c);
+                        debug("TOTAL", f.rows.length);
+                        if ( f.rows.length != 0 ) {
+                            debug("SUCESSO", 'Redirecionando para o login.');
+                            _configuracoes.verifica_usuarios();
+                        } else {
+                            debug("ERROR", 'Criando tabelas.');
+                            _configuracoes.create_tabelas();
+                        }
+                    },
+                    function( g, f ) {
+                        debug("QUERY", c);
+                        debug("ERROR", f.message);
+                        _configuracoes.verifica_tabelas();
+                    });
         });
-    }
-
-    function check_usuario_root() {
+    },
+    verifica_usuarios : function() {
         var c = 'SELECT usuario FROM usuarios WHERE usuario="root";';
         db.transaction(function( e ) {
-            e.executeSql(c, [ ], function( g, f ) {
-                debug("SUCESSO", c);
-                debug("TOTAL", f.rows.length);
-                if ( f.rows.length == 0 ) {
-                    debug("SUCESSO", 'Criando usuário.');
-                    definir_usuario_administradores();
-                } else {
-                    debug("ERROR", 'Redirecionando.');
-                    _constant.redirect("login.html");
-                }
-            }, function( g, f ) {
-                debug("ERROR", f.message);
-                verificar_tabelas();
+            e.executeSql(c, [ ],
+                    function( g, f ) {
+                        debug("QUERY", c);
+                        debug("TOTAL", f.rows.length);
+                        if ( f.rows.length == 0 ) {
+                            debug("SUCESSO", 'Criando usuário.');
+                            _configuracoes.add_usuarios();
+                        } else {
+                            debug("ERROR", 'Verificando chaves.');
+                            _configuracoes.verifica_chave();
+                        }
+                    },
+                    function( g, f ) {
+                        debug("QUERY", c);
+                        debug("ERROR", f.message);
+                        _configuracoes.verifica_tabelas();
+                    });
+        });
+    },
+    verifica_chave : function() {
+        var c = 'SELECT codigo_cliente FROM empresas;';
+        db.transaction(function( e ) {
+            e.executeSql(c, [ ],
+                    function( g, f ) {
+                        debug("QUERY", c);
+                        debug("TOTAL", f.rows.length);
+                        if ( f.rows.length == 0 ) {
+                            debug("SUCESSO", 'Redirecionando para registro de chaves.');
+                            _constant.redirect("atualizacoes_ativacao.html");
+                        } else {
+                            if ( f.rows.item(0).codigo_cliente == '' ) {
+                                debug("SUCESSO", 'Redirecionando para registro de chaves.');
+                                _constant.redirect("atualizacoes_ativacao.html");
+                            } else {
+                                debug("SUCESSO", 'Redirecionando para login.');
+                                _constant.redirect("login.html");
+                            }
 
-            });
+                        }
+                    },
+                    function( g, f ) {
+                        debug("QUERY", c);
+                        debug("ERROR", f.message);
+                        _configuracoes.verifica_tabelas();
+                    });
         });
     }
-
-}
-
+};
